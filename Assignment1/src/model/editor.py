a1f7b94e24263b40300ac59d02567de3ffe52da9
LisaHollaus <152526768+LisaHollaus@users.noreply.github.com>
2024-04-13 22:54:04 +0200

    Delete old Assignment1 directory

--- Assignment1/src/model/editor.py
+++
@@ -1,8 +0,0 @@
-from .subscriber import Subscriber
-
-
-# editor inherits Subscriber
-class Editor(Subscriber):
-    def __init__(self, ID: int, name: str, address: str):
-        super().__init__(ID, name, address)
-        # also inherits issue_list and newspaper_list
--- Assignment1/start.py
+++
@@ -1,5 +0,0 @@
-from src.app import create_app
-
-if __name__ == '__main__':
-    app = create_app()
-    create_app().run(debug=False, port=7890)
